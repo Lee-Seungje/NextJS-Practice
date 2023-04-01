5fb4255aec533ad09b5ab53087cdff1f0e286e6d
Lee-Seungje <moondgod@gmail.com>
2023-04-01 23:42:03 +0900

    Add post code

--- nextjs-intro/pages/about.tsx
+++ nextjs-intro/pages/about.tsx
@@ -1,10 +1,23 @@
+import axios from "axios";
 import Seo from "../components/Seo";
 
 const About = () => {
+  const get = async () => {
+    try {
+      const res = await axios.get(
+        "https://port-0-idea-archive-server-p8xrq2mlfszgkzn.sel3.cloudtype.app/post"
+      );
+      const data = res.data;
+      console.log(data);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   return (
     <div>
       <Seo title="About" />
-      <h1>About</h1>
+      <h1 onClick={get}>About</h1>
     </div>
   );
 };
